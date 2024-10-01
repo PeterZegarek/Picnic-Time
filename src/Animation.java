55c3eb4d7838b85388e566d95b36ae112323fc03
PeterZegarek <petrzegarek@gmail.com>
2024-10-01 18:08:51 -0400

    better seesaw animation

--- src/Animation.java
+++ src/Animation.java
@@ -8,11 +8,7 @@ import javax.swing.*;
 public class Animation extends JPanel {
 
     private int frameNumber;
-    private long elapsedTimeMillis;
 
-    private int anTimer;
-
-    private float pixelSize;
     public static void main(String[] args) {
         // set up window and default settings for it
         JFrame window;
@@ -28,12 +24,9 @@ public class Animation extends JPanel {
                 (screen.height - window.getHeight()) / 2);
         // set up timer
         Timer animationTimer;
-        final long startTime = System.currentTimeMillis();
         animationTimer = new Timer(16, new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 panel.frameNumber++;
-                panel.anTimer += 2;
-                panel.elapsedTimeMillis = System.currentTimeMillis() - startTime;
                 panel.repaint();
             }
         });
@@ -60,12 +53,14 @@ public class Animation extends JPanel {
         // draw different parts of scene
         drawBackground(g2);
         drawPicnic(g2);
-        drawScene(g2);
+        drawTrees(g2);
+        drawBird(g2);
         drawSeeSaw(g2);
     }
 
     private void drawSeeSaw(Graphics2D g2)
     {
+        // make seesaw itself
         Stroke originalStroke = g2.getStroke();
         g2.setPaint(Color.GRAY);
 
@@ -80,19 +75,22 @@ public class Animation extends JPanel {
         double seesawY = -10;
         double width = 12;
         double height = 0.3;
-    
-
 
         Color purple = new Color(204, 108, 231);
         g2.setPaint(purple);
 
-        int frame = (int) (Math.sin((Math.toRadians(anTimer))) * 20);
-        System.out.println(frame);
+        // frame oscillates from -20 to 20
+        int frame = (int) (Math.sin((Math.toRadians(frameNumber * 2.3))) * 20);
+
+        // rotate the plane of g2 - this affects all future drawings which is how the people stay on seesaw
         g2.rotate(Math.toRadians(frame), seesawX + width / 2, seesawY + height / 2);
 
         Rectangle2D.Double seesaw = new Rectangle2D.Double(seesawX, seesawY, width, height);
 
         g2.fill(seesaw);
+
+        // variable that will modify the knee bend of the people
+        double modifiedFrame = frame / 30.0;
        
         // make left person on seesaw
         g2.setStroke(new BasicStroke(0.1f));
@@ -101,69 +99,67 @@ public class Animation extends JPanel {
         g2.fill(person1Head);
         g2.setColor(Color.BLACK);
         g2.draw(person1Head);
-
-        // variable that will modify the knee bend of the people
-        double modifiedFrame = frame / 30.0;
-
         Line2D.Double firstBody = new Line2D.Double(-9.4, -8, -9.4, -10);
         g2.draw(firstBody);
-
         Line2D.Double firstArm1 = new Line2D.Double(-9.4, -9, -10.5, -8.5);
         g2.draw(firstArm1);
-
         Line2D.Double firstArm2 = new Line2D.Double(-9.4, -9, -8.3, -8.5);
         g2.draw(firstArm2);
 
-        if (modifiedFrame > 0) {
-            Line2D.Double firstKnee = new Line2D.Double(-9.4, -10, -8.7 + modifiedFrame, -10.9);
+        // draw knees / legs - moving when person is at bottom of seesaw
+        // it snaps a little, giving the illusion of jumping
+        // snapping happens because frames 1, 2, 3 are treated as if frame = 0
+        if (frame > 4) {
+            Line2D.Double firstKnee = new Line2D.Double(-9.4, -10, -9 + modifiedFrame, -10.9);
             g2.draw(firstKnee);
-            Line2D.Double firstLeg = new Line2D.Double(-8.7 + modifiedFrame, -10.9, -9.5, -11.8);
+            Line2D.Double firstLeg = new Line2D.Double(-9 + modifiedFrame, -10.9, -9.5, -11.8 + modifiedFrame);
         g2.draw(firstLeg);
         } else {
-            Line2D.Double firstKnee = new Line2D.Double(-9.4, -10, -8.7, -10.9);
+            Line2D.Double firstKnee = new Line2D.Double(-9.4, -10, -9, -10.9);
             g2.draw(firstKnee);
-            Line2D.Double firstLeg = new Line2D.Double(-8.7, -10.9, -9.5, -11.8);
+            Line2D.Double firstLeg = new Line2D.Double(-9, -10.9, -9.5, -11.8);
             g2.draw(firstLeg);
         }
 
+        // draw second person
         g2.setStroke(new BasicStroke(0.1f));
         g2.setColor(Color.WHITE);
         Ellipse2D.Double person2Head = new Ellipse2D.Double(-2, -8, 1.5, 1.5);
         g2.fill(person2Head);
         g2.setColor(Color.BLACK);
         g2.draw(person2Head);
-
         Line2D.Double secondBody = new Line2D.Double(-1.4, -8, -1.4, -10);
         g2.draw(secondBody);
-
         Line2D.Double secondArm1 = new Line2D.Double(-1.4, -9, -2.5, -8.5);
         g2.draw(secondArm1);
-
         Line2D.Double secondArm2 = new Line2D.Double(-1.4, -9, -.3, -8.5);
         g2.draw(secondArm2);
 
-        if (modifiedFrame < 0) {
-            Line2D.Double secondKnee = new Line2D.Double(-1.4, -10, -2.1 + modifiedFrame, -10.9);
+        // draw knees / legs - moving when person is at bottom of seesaw
+        // it snaps a little, giving the illusion of jumping
+        // snapping happens because frames -1, -2, -3 are treated as if frame = 0
+        if (frame < -4) {
+            Line2D.Double secondKnee = new Line2D.Double(-1.4, -10, -1.8 + modifiedFrame, -10.9);
             g2.draw(secondKnee);
-            Line2D.Double secondLeg = new Line2D.Double(-2.1 + modifiedFrame, -10.9, -1.5, -11.8);
+            Line2D.Double secondLeg = new Line2D.Double(-1.8 + modifiedFrame, -10.9, -1.5, -11.8 - modifiedFrame);
             g2.draw(secondLeg);
         } else {
-            Line2D.Double secondKnee = new Line2D.Double(-1.4, -10, -2.1, -10.9);
+            Line2D.Double secondKnee = new Line2D.Double(-1.4, -10, -1.8, -10.9);
             g2.draw(secondKnee);
-            Line2D.Double secondLeg = new Line2D.Double(-2.1, -10.9, -1.5, -11.8);
+            Line2D.Double secondLeg = new Line2D.Double(-1.8, -10.9, -1.5, -11.8);
             g2.draw(secondLeg);
         }
         
-
-
         g2.setStroke(originalStroke);
     }
 
+    // method to draw the picnic
     private void drawPicnic(Graphics2D g2)
     {
         AffineTransform original = g2.getTransform();
         Stroke originalStroke = g2.getStroke();
 
+        // draw blanket
         Color tan = new Color(214,181,136);
 
         g2.setPaint(tan);
@@ -176,6 +172,7 @@ public class Animation extends JPanel {
 
         Color brown = new Color(101, 79, 70);
 
+        // draw suitcase
         Rectangle2D.Double suitcase = new Rectangle2D.Double(7.5, -7.5, 2, 1.3);
         Arc2D.Double suitcaseArc = new Arc2D.Double(7.8, -6.4, 1.3, 0.5, 0, -180, Arc2D.OPEN);
 
@@ -187,10 +184,12 @@ public class Animation extends JPanel {
         g2.setPaint(brown);
         g2.draw(suitcaseArc);
 
+        // draw apple
         g2.setColor(Color.RED);
         Ellipse2D apple = new Ellipse2D.Double(6, -8.5, 0.3, 0.3);
         g2.fill(apple);
 
+        // draw person laying down
         g2.setColor(Color.WHITE);
         Ellipse2D.Double personHead = new Ellipse2D.Double(6.7, -9.4, 1.5, 1.5);
         g2.fill(personHead);
@@ -199,16 +198,14 @@ public class Animation extends JPanel {
 
         Line2D.Double body = new Line2D.Double(7.1, -9.3, 6, -11);
         g2.draw(body);
-
         Line2D.Double arm = new Line2D.Double(6.9, -9.6, 5.8, -9.6);
         g2.draw(arm);
-
         Line2D.Double knee = new Line2D.Double(6, -11, 5.3, -10.9);
         g2.draw(knee);
-
         Line2D.Double leg = new Line2D.Double(5.3, -10.9, 5.2, -11.4);
         g2.draw(leg);
 
+        // reset stroke size to typical
         g2.setStroke(originalStroke);
     }
 
@@ -227,20 +224,16 @@ public class Animation extends JPanel {
 
         //Painting the lake
         g2.setPaint(Color.blue);
-        
         Path2D path = new Path2D.Double();
         path.moveTo(-15, -0.5);
         path.lineTo(15, -0.5);
         path.append(new java.awt.geom.Arc2D.Double(-10, -7, 20,6, 0, 180, java.awt.geom.Arc2D.OPEN), true);
         path.closePath();
-
         g2.fill(path);
-
         g2.draw(path);
 
         // draw sun - with fluctuating alpha value
         int alphaEffect = (int) (Math.cos((Math.toRadians(frameNumber))) * 25);
-        //System.out.println(alphaEffect);
         g2.setPaint(new Color(255, 255, 0, 70 + alphaEffect));
         g2.fill(new Ellipse2D.Double(3.5, 10.25, 10, 10));
         g2.setPaint(new Color(255, 255, 0, 70 + alphaEffect));
@@ -251,32 +244,6 @@ public class Animation extends JPanel {
         g2.fill(new Ellipse2D.Double(5, 12.5, 7, 7));
         g2.setPaint(new Color(255, 255, 0, 70 + alphaEffect));
         g2.fill(new Ellipse2D.Double(5.5, 13.25, 6, 6));
-        
-
-        // draw a dot at each coordinate
-        // useful tool to flick on/off
-        // g2.setPaint(Color.BLACK);
-        // for (int i = 0; i <= 30; i++) {
-        //     for (int j = 0; j <= 30; j++) {
-        //         if (j == 15) {
-        //             g2.setPaint(Color.GRAY);
-        //             g2.fill(new Ellipse2D.Double(i-15, j-15, .5, .5));
-        //         } else if (i == 15){
-        //             g2.setPaint(Color.WHITE);
-        //             g2.fill(new Ellipse2D.Double(i-15, j-15, .5, .5));
-        //         } else {
-        //             g2.setPaint(Color.BLACK);
-        //             g2.fill(new Ellipse2D.Double(i-15, j-15, .5, .5));
-        //         }
-                
-        //     }
-        // }
-
-    }
-
-    private void drawScene(Graphics2D g2) {
-        drawTrees(g2);
-        drawBird(g2);
     }
 
     private void drawBird(Graphics2D g2) {
@@ -301,20 +268,20 @@ public class Animation extends JPanel {
     private void drawTrees(Graphics2D g2) {
         // left side tree
         // tree trunk
-        Path2D polyLeft = new Path2D.Double();
-        polyLeft.moveTo(-12, -3);
-        polyLeft.lineTo(-13, -12);
-        polyLeft.lineTo(-13.5, -12.5);
-        polyLeft.lineTo(-12.5, -12);
-        polyLeft.lineTo(-11, -13);
-        polyLeft.lineTo(-10, -12);
-        polyLeft.lineTo(-8.5, -12.5);
-        polyLeft.lineTo(-9, -12);
-        polyLeft.lineTo(-10, -3);
-        polyLeft.closePath();
+        Path2D leftTree = new Path2D.Double();
+        leftTree.moveTo(-12, -3);
+        leftTree.lineTo(-13, -12);
+        leftTree.lineTo(-13.5, -12.5);
+        leftTree.lineTo(-12.5, -12);
+        leftTree.lineTo(-11, -13);
+        leftTree.lineTo(-10, -12);
+        leftTree.lineTo(-8.5, -12.5);
+        leftTree.lineTo(-9, -12);
+        leftTree.lineTo(-10, -3);
+        leftTree.closePath();
         // make tree trunk brown
         g2.setPaint(new Color(150, 75, 0));
-        g2.fill(polyLeft);
+        g2.fill(leftTree);
 
         // leaves
         g2.setPaint(Color.decode("#2A7E19"));
@@ -322,20 +289,20 @@ public class Animation extends JPanel {
         
         // right side tree
         // tree trunk
-        Path2D polyRight = new Path2D.Double();
-        polyRight.moveTo(12, -3);
-        polyRight.lineTo(13, -12);
-        polyRight.lineTo(13.5, -12.5);
-        polyRight.lineTo(12.5, -12);
-        polyRight.lineTo(11, -13);
-        polyRight.lineTo(10, -12);
-        polyRight.lineTo(8.5, -12.5);
-        polyRight.lineTo(9, -12);
-        polyRight.lineTo(10, -3);
-        polyRight.closePath();
+        Path2D rightTree = new Path2D.Double();
+        rightTree.moveTo(12, -3);
+        rightTree.lineTo(13, -12);
+        rightTree.lineTo(13.5, -12.5);
+        rightTree.lineTo(12.5, -12);
+        rightTree.lineTo(11, -13);
+        rightTree.lineTo(10, -12);
+        rightTree.lineTo(8.5, -12.5);
+        rightTree.lineTo(9, -12);
+        rightTree.lineTo(10, -3);
+        rightTree.closePath();
         // make tree trunk brown
         g2.setPaint(new Color(150, 75, 0));
-        g2.fill(polyRight);
+        g2.fill(rightTree);
 
         // leaves
         g2.setPaint(Color.decode("#2A7E19"));
@@ -369,8 +336,5 @@ public class Animation extends JPanel {
         }
         g2.scale( width / (right-left), height / (bottom-top) );
         g2.translate( -left, -top );
-        double pixelWidth = Math.abs(( right - left ) / width);
-        double pixelHeight = Math.abs(( bottom - top ) / height);
-        pixelSize = (float)Math.max(pixelWidth,pixelHeight);
     }
 }
